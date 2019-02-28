1b4fac0d698d9289b48991aa48a70b8812fc84b8
HyeongTak <andy395023@gmail.com>
2019-03-01 05:04:58 +0900

    라우터

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const Koa = require('Koa');
-const app = new Koa();
+const Router = require('koa-router');
 
-app.use(async ctx => {
-    ctx.body = "hello, world!";
+const app = new Koa();
+const router = new Router();
+
+const port = 3000;
+
+// 라우터 세팅
+router.get('/', (ctx)=> {
+    ctx.body = '홈 라우터';
 });
 
-app.listen(3000);
+router.get('/help', (ctx) => {
+    ctx.body = '도움말 라우터';
+});
+
+// 라우터 사용 선언
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(port, () => {
+  console.log(`Koa Server on ${port}port`);
+});
